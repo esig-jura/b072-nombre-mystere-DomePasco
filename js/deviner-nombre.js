ca05f64f618bcee6929a7eff0c7a1c6d143fd7a6
domenico.pascale <domenico.pascale@divcom.ch>
2023-02-08 10:45:24 +0100

    création boucle do..while

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,36 @@
 /**
  * Jeu du nombre mystère
- * @author  Steve Fallet
+ * @author  Domenico Pascale
  * @version 1.0
- * @since   2022-08-30 (date de création)
+ * @since   2032-01-08 (date de création)
  */
 
-//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    'use strict';
+"use strict";
+// TODO afficher les messages d'erreur en HTML et pas avec alert
+// TODO panneau de config pour modifer MIN et MAX
+/**
+ * Fonction qui retourne un nombre aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function tireNombre(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+console.log(tireNombre(1,100));
 
-   // Ton code de malade ici ...
+let nbMystere = tireNombre(1, 100);
 
-}()); //main IIFE
+let reponse = Number(prompt("Entrez un nombre situé entre 1 et 100"));
+
+do {
+    if (reponse < nbMystere) {
+        prompt("Plus grand !")
+    }
+    else {
+        prompt("Plus petit !")
+    }
+
+} while (reponse !== nbMystere)
+
+
